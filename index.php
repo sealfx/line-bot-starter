fbd0c1e2d9e4d07fc297dae4fcf088cd13cd2108
sealfx <sealfx@yahoo.com>
2021-07-29 17:23:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
-<?php
-http_response_code(200);
-
-include "src/sendToLine.php";
-include "src/config.php";
-
-
-if ($botText == 'ทดสอบ') {
-    $text = 'ทดสอบตอบกลับข้อความ';
-    sendFormatTextMessage($LINEDatas, $replyToken, $text);
-}
-
-function sendFormatTextMessage($LINEDatas, $replyToken, $text)
-{
-
-    $str = '{
-        "replyToken":"'.$replyToken.'",
-        "messages":
-        [
-            {
-                "type":"text",
-                "text":"'.$text.'"
-            }
-        ]
-    }';
-
-    sentMessage($str,$LINEDatas);
-}
+<?php 
+ /*Return HTTP Request 200*/
+ http_response_code(200);
+?>
