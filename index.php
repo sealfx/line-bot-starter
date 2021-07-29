47f7ddc68f03434cf47e1dc2567a1b964ed94056
sealfx <sealfx@yahoo.com>
2021-07-29 16:52:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
-<?php 
- /*Return HTTP Request 200*/
- http_response_code(200);
-?>
+<?php
+http_response_code(200);
+
+include "src/sendToLine.php";
+include "src/config.php";
+
+
+if ($botText == 'ทดสอบ') {
+    $text = 'ทดสอบตอบกลับข้อความ';
+    sendFormatTextMessage($LINEDatas, $replyToken, $text);
+}
+
+function sendFormatTextMessage($LINEDatas, $replyToken, $text)
+{
+
+    $str = '{
+        "replyToken":"'.$replyToken.'",
+        "messages":
+        [
+            {
+                "type":"text",
+                "text":"'.$text.'"
+            }
+        ]
+    }';
+
+    sentMessage($str,$LINEDatas);
+}
